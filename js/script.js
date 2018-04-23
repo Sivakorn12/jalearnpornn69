e3e049e7b3010b8a93625c2b66e4eba60f3818a4
nattawoot <spw.ntw@gmail.com>
2018-04-23 11:32:31 +0700

    second ajax realtime

--- js/script.js
+++ js/script.js
@@ -1,15 +1,4 @@
-$('#booking-detail').on('show.bs.modal', function(event) {
-  var button = $(event.relatedTarget)
-  var id = button.data('id'); 
-  var modal = $(this)
-  $.ajax({
-      url: window.location.pathname + "/view/" + id,
-      type: 'GET',
-      dataType: 'JSON',
-      data: { id: button.data('id') },
-      success: function(data) {
-          modal.find('#showView').html(data.html);
-      }
-  });
-});
+function test() {
+  
+}
 

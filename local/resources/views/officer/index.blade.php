97f199fdc7f416bc9589fad3c116e6d7d5e41583
nattawoot <spw.ntw@gmail.com>
2018-05-08 00:32:24 +0700

    add holiday function

--- local/resources/views/officer/index.blade.php
+++ local/resources/views/officer/index.blade.php
@@ -5,7 +5,6 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.2.7/fullcalendar.min.js"></script>  
 <script type="text/javascript" src="{{ url('js/fullcalendar.th.js')}}"></script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.2.7/fullcalendar.min.css" />
- 
     <div class="row">
       <div class="col-xs-12" style="padding-bottom:10px">
               <div class="panel panel-primary" style="width:100%!important">  

e0d8a102aa88275fec14769cb74b75b60b0b98f3
nattawoot <spw.ntw@gmail.com>
2018-05-10 09:45:45 +0700

    fixed title

--- local/resources/views/layouts/officer.blade.php
+++ local/resources/views/layouts/officer.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>ระบบการจองและการใช้ห้องประชุมออนไลน์</title>
 
     <!-- Styles -->
     <link href="{{ url('css/bootstrap.css') }}" rel="stylesheet">
